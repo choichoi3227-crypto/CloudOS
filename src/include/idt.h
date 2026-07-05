bea252b14f42796884379704d12ca750e1dca306
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:43:02 +0900

    Update idt.h

--- src/include/idt.h
+++ src/include/idt.h
@@ -1,23 +1,18 @@
 #ifndef IDT_H
 #define IDT_H
 #include "types.h"
+#include "task.h"
 
 struct idt_entry {
-    uint16_t base_low;
-    uint16_t selector;
-    uint8_t ist;
-    uint8_t flags;
-    uint16_t base_mid;
-    uint32_t base_high;
-    uint32_t zero;
+    uint16_t base_low; uint16_t selector; uint8_t ist; uint8_t flags;
+    uint16_t base_mid; uint32_t base_high; uint32_t zero;
 } __attribute__((packed));
 
 struct idt_ptr {
-    uint16_t limit;
-    uint64_t base;
+    uint16_t limit; uint64_t base;
 } __attribute__((packed));
 
 void idt_init(void);
-void irq_handler_c(uint64_t* rsp);
+void irq_handler_c(struct registers* regs);
 
 #endif
