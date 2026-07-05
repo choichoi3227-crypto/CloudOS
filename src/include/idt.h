5fb4c91305853ba3ac859d148cccab323f75c77a
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:49:29 +0900

    Define registers struct and declare asm_irq_handler
    Added a struct for CPU registers and an external assembly IRQ handler declaration.

--- src/include/idt.h
+++ src/include/idt.h
@@ -1,7 +1,13 @@
 #ifndef IDT_H
 #define IDT_H
 #include "types.h"
-#include "task.h"
+
+struct registers {
+    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
+    uint64_t rdi, rsi, rbp, rdx, rcx, rbx, rax;
+    uint64_t int_no, err_code;
+    uint64_t rip, cs, rflags, rsp, ss;
+};
 
 struct idt_entry {
     uint16_t base_low; uint16_t selector; uint8_t ist; uint8_t flags;
@@ -14,5 +20,6 @@ struct idt_ptr {
 
 void idt_init(void);
 void irq_handler_c(struct registers* regs);
+extern void asm_irq_handler();
 
 #endif
