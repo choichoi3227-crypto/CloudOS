20bb5782e3ce63100403da3b0ac385863a5c0344
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 07:00:13 +0900

    Update vmm.h

--- src/include/vmm.h
+++ src/include/vmm.h
@@ -5,8 +5,11 @@
 
 #define PAGE_PRESENT 0x1
 #define PAGE_WRITABLE 0x2
+#define PAGE_USER 0x4
 
 void vmm_init(void);
 void* vmm_map_page(physaddr_t phys, virtaddr_t virt, uint32_t flags);
+uint64_t vmm_create_user_pml4(void);
+void vmm_map_page_to_pml4(uint64_t pml4, physaddr_t phys, virtaddr_t virt, uint32_t flags);
 
 #endif
