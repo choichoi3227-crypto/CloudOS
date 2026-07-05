723f5b04b7811cc9ac3ab2cfd08e41ead2719a8a
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:30:24 +0900

    Update keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,10 +1,10 @@
 #include "keyboard.h"
+#include "io.h"
 
 static char kb_buffer[256];
 static int kb_head = 0;
 static int kb_tail = 0;
 
-// Set 1 스캔코드 -> ASCII 매핑
 const char scancode_to_ascii[128] = {
     0, 27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b',
     '\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',
@@ -32,7 +32,7 @@ void keyboard_handle_scancode(uint8_t scancode) {
 
 char keyboard_getchar(void) {
     while (kb_head == kb_tail) {
-        __asm__ __volatile__("hlt"); // 입력 대기 중 CPU 절전
+        __asm__ __volatile__("hlt");
     }
     char c = kb_buffer[kb_tail];
     kb_tail = (kb_tail + 1) % 256;
