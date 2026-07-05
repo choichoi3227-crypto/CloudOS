302f62a7bda306f8424546a3a7da76c86388476e
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:44:01 +0900

    Update interrupt.asm

--- src/interrupt.asm
+++ src/interrupt.asm
@@ -4,39 +4,40 @@ extern irq_handler_c
 
 global asm_irq_handler
 asm_irq_handler:
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push rbp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
     push r15
-
-    mov rdi, rsp
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rbp
+    push rdi
+    push rsi
+    push rdx
+    push rcx
+    push rbx
+    push rax
+    
+    mov rdi, rsp  ; 레지스터 프레임의 주소를 첫 번째 인자로 전달
     call irq_handler_c
-
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rbp
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
+    
+    ; 컨텍스트 스위칭 후 돌아올 때 스택(rsp)은 새 태스크의 스택으로 바뀌어 있음
     pop rax
+    pop rbx
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rbp
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
 
     iretq
