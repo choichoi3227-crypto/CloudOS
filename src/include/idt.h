67e943ca8dd2863c9994f28f321449397d70f40d
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:29:26 +0900

    Update and rename src/idt.h to src/include/idt.h

---
+++ src/include/idt.h
@@ -0,0 +1,23 @@
+#ifndef IDT_H
+#define IDT_H
+#include "types.h"
+
+struct idt_entry {
+    uint16_t base_low;
+    uint16_t selector;
+    uint8_t ist;
+    uint8_t flags;
+    uint16_t base_mid;
+    uint32_t base_high;
+    uint32_t zero;
+} __attribute__((packed));
+
+struct idt_ptr {
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed));
+
+void idt_init(void);
+void irq_handler_c(uint64_t* rsp);
+
+#endif
