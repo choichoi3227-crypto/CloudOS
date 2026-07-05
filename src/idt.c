4739ed47b5b5688f9e0a2c34cd06389e48e0cfc4
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:29:43 +0900

    Update idt.c

--- src/idt.c
+++ src/idt.c
@@ -1,5 +1,6 @@
 #include "idt.h"
-#include "vga.h"
+#include "io.h"
+#include "keyboard.h"
 
 struct idt_entry idt[256];
 struct idt_ptr idt_ptr;
@@ -10,7 +11,7 @@ void idt_set_gate(int n, uint64_t handler) {
     idt[n].base_high = (handler >> 32) & 0xFFFFFFFF;
     idt[n].selector = 0x08;
     idt[n].ist = 0;
-    idt[n].flags = 0x8E;
+    idt[n].flags = 0x8E; // Present, Ring 0, Interrupt Gate
     idt[n].zero = 0;
 }
 
@@ -26,25 +27,23 @@ void idt_init(void) {
     
     __asm__ __volatile__("lidt %0" : : "m"(idt_ptr));
     
-    // PIC 리매핑 (IRQ 0-15를 인터럽트 32-47로 이동)
-    outb(0x20, 0x11);
-    outb(0xA0, 0x11);
-    outb(0x21, 0x20);
-    outb(0xA1, 0x28);
-    outb(0x21, 0x04);
-    outb(0xA1, 0x02);
-    outb(0x21, 0x01);
-    outb(0xA1, 0x01);
-    outb(0x21, 0x00);
-    outb(0xA1, 0x00);
+    // PIC 리매핑 (IRQ 0-15 -> 인터럽트 32-47)
+    outb(0x20, 0x11); io_wait();
+    outb(0xA0, 0x11); io_wait();
+    outb(0x21, 0x20); io_wait();
+    outb(0xA1, 0x28); io_wait();
+    outb(0x21, 0x04); io_wait();
+    outb(0xA1, 0x02); io_wait();
+    outb(0x21, 0x01); io_wait();
+    outb(0xA1, 0x01); io_wait();
+    outb(0x21, 0x00); io_wait();
+    outb(0xA1, 0x00); io_wait();
 }
 
 void irq_handler_c(uint64_t* rsp) {
-    // 인터럽트 번호 확인 (스택에 푸시된 에러 코드와 인터럽트 번호 읽기)
-    // 이 간략화된 구조에서는 키보드 IRQ1(벡터 33)을 가정
     uint8_t scancode = inb(0x60);
     keyboard_handle_scancode(scancode);
     
-    // EOI(End of Interrupt) 전송
+    // EOI 전송
     outb(0x20, 0x20);
 }
