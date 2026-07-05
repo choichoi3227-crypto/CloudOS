73177c572743987a73c1688a04d3b61d93f8415f
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:50:51 +0900

    Update string.c

--- src/string.c
+++ src/string.c
@@ -10,3 +10,10 @@ uint64_t strlen(const char* str) {
     while (str[len]) len++;
     return len;
 }
+
+void* memcpy(void* dest, const void* src, uint64_t n) {
+    uint8_t* d = (uint8_t*)dest;
+    const uint8_t* s = (const uint8_t*)src;
+    while (n--) *d++ = *s++;
+    return dest;
+}
