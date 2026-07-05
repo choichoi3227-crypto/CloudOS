426fc589ee924971451694f6e9da362d6947a688
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 06:57:25 +0900

    Update vfs.h

--- src/include/vfs.h
+++ src/include/vfs.h
@@ -2,10 +2,9 @@
 #define VFS_H
 #include "types.h"
 
-#define MAX_FILES 32
-
 void vfs_init(void);
 void vfs_write_file(const char* name, void* buf, uint32_t size);
 uint32_t vfs_read_file(const char* name, void* buf, uint32_t size);
+void vfs_list_files(void);
 
 #endif
