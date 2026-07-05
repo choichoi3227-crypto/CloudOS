ddf9fe123b607cf919a68b1c955fff8fb6a2cf8c
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 06:57:59 +0900

    Add vfs_list_files function to list files

--- src/vfs.c
+++ src/vfs.c
@@ -12,3 +12,7 @@ void vfs_write_file(const char* name, void* buf, uint32_t size) {
 uint32_t vfs_read_file(const char* name, void* buf, uint32_t size) {
     return cloudfs_read_file(name, buf, size);
 }
+
+void vfs_list_files(void) {
+    cloudfs_list_files();
+}
