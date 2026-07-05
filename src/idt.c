6fac0396f4263d1d85e71023f34661459be2e997
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-05 18:43:16 +0900

    Update idt.c

--- src/idt.c
+++ src/idt.c
@@ -1,6 +1,9 @@
 #include "idt.h"
 #include "io.h"
 #include "keyboard.h"
+#include "timer.h"
+#include "task.h"
+#include "vga.h"
 
 struct idt_entry idt[256];
 struct idt_ptr idt_ptr;
@@ -11,7 +14,7 @@ void idt_set_gate(int n, uint64_t handler) {
     idt[n].base_high = (handler >> 32) & 0xFFFFFFFF;
     idt[n].selector = 0x08;
     idt[n].ist = 0;
-    idt[n].flags = 0x8E; // Present, Ring 0, Interrupt Gate
+    idt[n].flags = 0x8E;
     idt[n].zero = 0;
 }
 
@@ -20,30 +23,26 @@ extern void asm_irq_handler();
 void idt_init(void) {
     idt_ptr.limit = sizeof(struct idt_entry) * 256 - 1;
     idt_ptr.base = (uint64_t)&idt;
-    
-    for (int i = 0; i < 256; i++) {
-        idt_set_gate(i, (uint64_t)asm_irq_handler);
-    }
-    
+    for (int i = 0; i < 256; i++) idt_set_gate(i, (uint64_t)asm_irq_handler);
     __asm__ __volatile__("lidt %0" : : "m"(idt_ptr));
     
-    // PIC 리매핑 (IRQ 0-15 -> 인터럽트 32-47)
-    outb(0x20, 0x11); io_wait();
-    outb(0xA0, 0x11); io_wait();
-    outb(0x21, 0x20); io_wait();
-    outb(0xA1, 0x28); io_wait();
-    outb(0x21, 0x04); io_wait();
-    outb(0xA1, 0x02); io_wait();
-    outb(0x21, 0x01); io_wait();
-    outb(0xA1, 0x01); io_wait();
-    outb(0x21, 0x00); io_wait();
-    outb(0xA1, 0x00); io_wait();
+    // PIC 리매핑
+    outb(0x20, 0x11); io_wait(); outb(0xA0, 0x11); io_wait();
+    outb(0x21, 0x20); io_wait(); outb(0xA1, 0x28); io_wait();
+    outb(0x21, 0x04); io_wait(); outb(0xA1, 0x02); io_wait();
+    outb(0x21, 0x01); io_wait(); outb(0xA1, 0x01); io_wait();
+    outb(0x21, 0x00); io_wait(); outb(0xA1, 0x00); io_wait();
 }
 
-void irq_handler_c(uint64_t* rsp) {
-    uint8_t scancode = inb(0x60);
-    keyboard_handle_scancode(scancode);
-    
-    // EOI 전송
-    outb(0x20, 0x20);
+void irq_handler_c(struct registers* regs) {
+    if (regs->int_no == 32) { // Timer IRQ0
+        schedule(regs);       // 프로세스 스케줄링
+        timer_handler();
+        outb(0x20, 0x20);
+    } else if (regs->int_no == 33) { // Keyboard IRQ1
+        keyboard_handle_scancode(inb(0x60));
+        outb(0x20, 0x20);
+    } else {
+        outb(0x20, 0x20);
+    }
 }
