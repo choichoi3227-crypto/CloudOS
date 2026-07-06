943ec65aeeb35aaecfd07dd1125eb71189f9a107
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:26:10 +0900

    Update ahci.h

--- src/include/ahci.h
+++ src/include/ahci.h
@@ -2,7 +2,7 @@
 #define AHCI_H
 #include "types.h"
 
-#define AHCI_BASE 0x400000 // AHCI MMIO 가상 주소 매핑 지점
+#define AHCI_BASE 0x400000
 
 void ahci_init(void);
 int ahci_read_sectors(uint64_t lba, uint32_t count, void* buf);
