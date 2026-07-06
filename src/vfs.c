2bf68c4701c5cf5b6275b27c35390901f5d994bd
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:27:20 +0900

    Update vfs.c

--- src/vfs.c
+++ src/vfs.c
@@ -1,18 +1,7 @@
 #include "vfs.h"
 #include "cloudfs.h"
 
-void vfs_init(void) {
-    cloudfs_init();
-}
-
-void vfs_write_file(const char* name, void* buf, uint32_t size) {
-    cloudfs_write_file(name, buf, size);
-}
-
-uint32_t vfs_read_file(const char* name, void* buf, uint32_t size) {
-    return cloudfs_read_file(name, buf, size);
-}
-
-void vfs_list_files(void) {
-    cloudfs_list_files();
-}
+void vfs_init(void) { cloudfs_init(); }
+void vfs_write_file(const char* name, void* buf, uint32_t size) { cloudfs_write_file(name, buf, size); }
+uint32_t vfs_read_file(const char* name, void* buf, uint32_t size) { return cloudfs_read_file(name, buf, size); }
+void vfs_list_files(void) { cloudfs_list_files(); }
