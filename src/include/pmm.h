b950fb203a9ad4a604601922f8bf349cbb336ffb
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-07 06:59:45 +0900

    Update pmm.h

--- src/include/pmm.h
+++ src/include/pmm.h
@@ -2,7 +2,8 @@
 #define PMM_H
 #include "types.h"
 
-void pmm_init(uint32_t* mmap_addr, uint32_t mmap_length);
+// 인자를 Multiboot2 정보 구조체 포인터(void*)로 변경
+void pmm_init(void* mb_info);
 uint32_t pmm_alloc_block(void);
 
 #endif
