bbb9f44775b01551bc39d0240cce57d5cc96ca9b
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-07 07:14:31 +0900

    Update idt.h

--- src/include/idt.h
+++ src/include/idt.h
@@ -1,31 +1,9 @@
 #ifndef IDT_H
 #define IDT_H
-#include "types.h"
-
-// interrupt.asm의 push 순서와 정확히 일치
-struct registers {
-    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
-    uint64_t rbp, rdi, rsi, rdx, rcx, rbx, rax;
-    uint64_t int_no, err_code;
-    uint64_t rip, cs, rflags, rsp, ss;
-};
-
-struct idt_entry {
-    uint16_t base_low;
-    uint16_t selector;
-    uint8_t ist;
-    uint8_t flags;
-    uint16_t base_mid;
-    uint32_t base_high;
-    uint32_t zero;
-} __attribute__((packed));
-
-struct idt_ptr {
-    uint16_t limit;
-    uint64_t base;
-} __attribute__((packed));
-
+#include <types.h>
+struct registers { uint64_t r15, r14, r13, r12, r11, r10, r9, r8, rbp, rdi, rsi, rdx, rcx, rbx, rax, int_no, err_code, rip, cs, rflags, rsp, ss; };
+struct idt_entry { uint16_t base_low; uint16_t selector; uint8_t ist; uint8_t flags; uint16_t base_mid; uint32_t base_high; uint32_t zero; } __attribute__((packed));
+struct idt_ptr { uint16_t limit; uint64_t base; } __attribute__((packed));
 void idt_init(void);
-extern void asm_irq_handler();
-
+void irq_handler_c(struct registers* regs);
 #endif
