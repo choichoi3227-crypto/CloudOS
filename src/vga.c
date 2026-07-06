77e4f204586313f039e562526fd628e5ae84046a
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 16:43:39 +0900

    Update vga.c

--- src/vga.c
+++ src/vga.c
@@ -8,9 +8,9 @@ static uint8_t vga_color = 0x0F;
 void vga_init(void) {
     vga_row = 0;
     vga_col = 0;
-    for (int y = 0; y < VGA_HEIGHT; y++) {
-        for (int x = 0; x < VGA_WIDTH; x++) {
-            VGA_MEMORY[y * VGA_WIDTH + x] = ((uint16_t)vga_color << 8) | ' ';
+    for (int y = 0; y < 25; y++) {
+        for (int x = 0; x < 80; x++) {
+            VGA_MEMORY[y * 80 + x] = ((uint16_t)vga_color << 8) | ' ';
         }
     }
 }
@@ -24,15 +24,15 @@ void vga_print_char(char c) {
         vga_col = 0;
         vga_row++;
     } else {
-        VGA_MEMORY[vga_row * VGA_WIDTH + vga_col] = ((uint16_t)vga_color << 8) | c;
+        VGA_MEMORY[vga_row * 80 + vga_col] = ((uint16_t)vga_color << 8) | c;
         vga_col++;
-        if (vga_col >= VGA_WIDTH) {
+        if (vga_col >= 80) {
             vga_col = 0;
             vga_row++;
         }
     }
-    if (vga_row >= VGA_HEIGHT) {
-        vga_init();
+    if (vga_row >= 25) {
+        vga_init(); // 간단화된 스크롤
     }
 }
 
