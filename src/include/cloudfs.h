ff5fe66a996f1fbd51dd8261fc40710b3ff0e4a8
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:26:36 +0900

    Fix formatting in cloudfs.h header file

--- src/include/cloudfs.h
+++ src/include/cloudfs.h
@@ -2,7 +2,7 @@
 #define CLOUDFS_H
 #include "types.h"
 
-#define CLOUDFS_MAGIC 0x434C4453 // "CLDS"
+#define CLOUDFS_MAGIC 0x434C4453
 #define SECTOR_SIZE 512
 #define INODE_SIZE 64
 #define MAX_INODES 64
@@ -20,7 +20,7 @@ typedef struct {
     char name[32];
     uint32_t size;
     uint32_t start_sector;
-    uint8_t type;   // 0: Empty, 1: File, 2: Directory
+    uint8_t type;
     uint8_t used;
 } cloudfs_inode_t;
 
