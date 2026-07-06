9c9e1732221cefaa8fa78c7bdb6ad44a82ae6ec3
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 16:00:45 +0900

    Create tcp.h

---
+++ src/include/tcp.h
@@ -0,0 +1,21 @@
+#ifndef TCP_H
+#define TCP_H
+#include "types.h"
+#include "net.h" // ipv4_header_t 등을 사용하기 위해
+
+typedef struct {
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint32_t seq;
+    uint32_t ack;
+    uint8_t data_offset;
+    uint8_t flags;
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgent;
+} __attribute__((packed)) tcp_header_t;
+
+void tcp_handle_packet(ipv4_header_t* ip, uint8_t* payload, uint32_t len);
+void tcp_send(uint8_t* dest_ip, uint16_t dest_port, uint32_t seq, uint32_t ack, uint8_t flags);
+
+#endif
