a1c326daf856b5e26494ce72131fa624799cfa1b
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:21:55 +0900

    Update string.c

--- src/string.c
+++ src/string.c
@@ -5,6 +5,14 @@ int strcmp(const char* a, const char* b) {
     return *(const unsigned char*)a - *(const unsigned char*)b;
 }
 
+int strncmp(const char* a, const char* b, uint64_t n) {
+    for (uint64_t i = 0; i < n; i++) {
+        if (a[i] != b[i]) return a[i] - b[i];
+        if (a[i] == '\0') return 0;
+    }
+    return 0;
+}
+
 uint64_t strlen(const char* str) {
     uint64_t len = 0;
     while (str[len]) len++;
@@ -16,4 +24,10 @@ void* memcpy(void* dest, const void* src, uint64_t n) {
     const uint8_t* s = (const uint8_t*)src;
     while (n--) *d++ = *s++;
     return dest;
+}
+
+void* memset(void* ptr, int value, uint64_t num) {
+    uint8_t* p = (uint8_t*)ptr;
+    while (num--) *p++ = (uint8_t)value;
+    return ptr;
 }
