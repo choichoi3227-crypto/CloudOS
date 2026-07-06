a4fd7eda62514eaa997d3493bbda797b004f3eb9
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-07 07:14:53 +0900

    Update keyboard.h

--- src/include/keyboard.h
+++ src/include/keyboard.h
@@ -1,9 +1,7 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
-#include "types.h"
-
+#include <types.h>
 void keyboard_init(void);
 void keyboard_handle_scancode(uint8_t scancode);
 char keyboard_getchar(void);
-
 #endif
