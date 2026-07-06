31fe350d676fd6b79d15b0f836187ba87b3851e1
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:35:30 +0900

    Create graphics.h

---
+++ src/include/graphics.h
@@ -0,0 +1,17 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include "types.h"
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define FONT_WIDTH 8
+#define FONT_HEIGHT 16
+
+void graphics_init(uint64_t fb_addr, uint32_t pitch);
+void draw_pixel(int x, int y, uint32_t color);
+void draw_rect(int x, int y, int width, int height, uint32_t color);
+void draw_char(char c, int x, int y, uint32_t color);
+void draw_string(const char* str, int x, int y, uint32_t color);
+void swap_buffers(void);
+
+#endif
