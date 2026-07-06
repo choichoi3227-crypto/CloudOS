77abdb52a4c0280cb6b06e53b0ccc880a9bbf94c
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:47:40 +0900

    Update timer.h

--- src/include/timer.h
+++ src/include/timer.h
@@ -1,6 +1,10 @@
 #ifndef TIMER_H
 #define TIMER_H
 #include "types.h"
+
+extern uint64_t timer_ticks; // 추가: 커널 전체에서 접근 가능하도록 선언
+
 void timer_init(uint32_t freq);
 void timer_handler(void);
+
 #endif
