aafbab2c6ae1b2137b6fafa96ed67e17311ee8cd
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:21:40 +0900

    Update string.h

--- src/include/string.h
+++ src/include/string.h
@@ -4,4 +4,6 @@
 int strcmp(const char* a, const char* b);
 uint64_t strlen(const char* str);
 void* memcpy(void* dest, const void* src, uint64_t n);
+void* memset(void* ptr, int value, uint64_t num);
+int strncmp(const char* a, const char* b, uint64_t n);
 #endif
