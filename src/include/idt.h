cbbd04e736766e35f3c81ffe44340992a42e849b
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 14:29:12 +0900

    Remove registers struct from idt.h
    Removed the 'registers' struct definition from idt.h.

--- src/include/idt.h
+++ src/include/idt.h
@@ -1,13 +1,7 @@
 #ifndef IDT_H
 #define IDT_H
 #include "types.h"
-
-struct registers {
-    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
-    uint64_t rdi, rsi, rbp, rdx, rcx, rbx, rax;
-    uint64_t int_no, err_code;
-    uint64_t rip, cs, rflags, rsp, ss;
-};
+#include "task.h"
 
 struct idt_entry {
     uint16_t base_low; uint16_t selector; uint8_t ist; uint8_t flags;
