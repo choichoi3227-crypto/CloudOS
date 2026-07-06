da9b8e5c23c91de4e12026230dcd800391cedb01
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-06 16:47:22 +0900

    Create vga.h

---
+++ src/vga.h
@@ -0,0 +1,9 @@
+#ifndef VGA_H
+#define VGA_H
+#include "types.h"
+enum vga_color { VGA_COLOR_BLACK=0, VGA_COLOR_WHITE=15, VGA_COLOR_LIGHT_CYAN=11 };
+void vga_init(void);
+void vga_set_color(uint8_t fg, uint8_t bg);
+void vga_print_char(char c);
+void vga_print(const char* str);
+#endif
