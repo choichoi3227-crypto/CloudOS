e2a527af5704c1feff41ef41aceca7b23f115644
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-07 06:49:37 +0900

    Update types.h

--- src/include/types.h
+++ src/include/types.h
@@ -4,12 +4,6 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
-
-typedef uint64_t size_t;
-typedef uint64_t physaddr_t;
-typedef uint64_t virtaddr_t;
-
-#define NULL ((void*)0)
 #define PAGE_SIZE 4096
-
+#define NULL ((void*)0)
 #endif
