eb76a3c1103470aad27abec986dba7b5a2756498
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-08 06:34:47 +0900

    Update interrupt.asm

--- src/interrupt.asm
+++ src/interrupt.asm
@@ -1,118 +1,29 @@
+; interrupt.asm
+; x86 하드웨어 인터럽트(IRQ)를 C 함수와 연결하는 어셈블리 스텁
+
 section .text
-bits 64
-extern irq_handler_c
 
-; 공통 인터럽트 핸들러 (에러 코드가 없는 경우 호출됨)
-global asm_common_irq_handler
-asm_common_irq_handler:
-    ; 가짜 에러 코드 0 푸시 (스택 프레임 통일)
-    push 0
-    ; 인터럽트 번호는 매크로를 통해 푸시됨
-    ; 레지스터 저장
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8
-    push rbp
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push rbx
-    push rax
-    
-    mov rdi, rsp
-    call irq_handler_c
-    
-    pop rax
-    pop rbx
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop rbp
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-    
-    ; 에러 코드와 인터럽트 번호 제거
-    add rsp, 16
-    iretq
+; 외부에 정의된 C 언어 핸들러 함수들
+extern keyboard_irq_handler
+extern mouse_irq_handler
 
-; 에러 코드가 있는 인터럽트 핸들러 (페이지 폴트 등)
-global asm_err_irq_handler
-asm_err_irq_handler:
-    ; 에러 코드는 이미 스택에 있음
-    push 0 ; 인터럽트 번호 자리 (매크로로 채움)
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8
-    push rbp
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push rbx
-    push rax
-    
-    mov rdi, rsp
-    call irq_handler_c
-    
-    pop rax
-    pop rbx
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop rbp
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-    
-    add rsp, 16
-    iretq
+; IRQ 1 (키보드) 핸들러 스텁
+global isr_stub_33
+isr_stub_33:
+    pushad              ; 레지스터들을 스택에 백업 (상용 OS 필수)
+    call keyboard_irq_handler
+    mov al, 0x20        ; PIC에게 인터럽트 처리 완료를 알림 (EOI)
+    out 0x20, al        ; 마스터 PIC 포트로 전송
+    popad               ; 레지스터 복구
+    iret                ; 인터럽트 반환
 
-; %1: 인터럽트 번호, %2: 핸들러 이름
-%macro DEFINE_IRQ 2
-global %2
-%2:
-    push %1           ; 인터럽트 번호 푸시
-    jmp asm_common_irq_handler
-%endmacro
-
-; 32~47번 인터럽트(타이머, 키보드 등) 핸들러 생성
-DEFINE_IRQ 32, irq32
-DEFINE_IRQ 33, irq33
-DEFINE_IRQ 34, irq34
-DEFINE_IRQ 35, irq35
-DEFINE_IRQ 36, irq36
-DEFINE_IRQ 37, irq37
-DEFINE_IRQ 38, irq38
-DEFINE_IRQ 39, irq39
-DEFINE_IRQ 40, irq40
-DEFINE_IRQ 41, irq41
-DEFINE_IRQ 42, irq42
-DEFINE_IRQ 43, irq43
-DEFINE_IRQ 44, irq44
-DEFINE_IRQ 45, irq45
-DEFINE_IRQ 46, irq46
-DEFINE_IRQ 47, irq47
+; IRQ 12 (마우스) 핸들러 스텁
+global isr_stub_44
+isr_stub_44:
+    pushad
+    call mouse_irq_handler
+    mov al, 0x20
+    out 0xA0, al        ; 슬레이브 PIC에게 EOI 전송 (마우스는 슬레이브에 연결됨)
+    out 0x20, al        ; 마스터 PIC에게 EOI 전송
+    popad
+    iret
