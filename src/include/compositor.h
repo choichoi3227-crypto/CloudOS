c748d606f6a3f8ce7fe581fc2e1d95c79740a6ab
choichoi3227-crypto <choichoi3227@gmail.com>
2026-07-07 15:46:45 +0900

    Update compositor.h

--- src/include/compositor.h
+++ src/include/compositor.h
@@ -1,22 +1,16 @@
+// compositor.h
 #ifndef COMPOSITOR_H
 #define COMPOSITOR_H
+
 #include "types.h"
+#include "wm_common.h" // wm_window_t 사용을 위해 포함
 
 #define MAX_VIRTUAL_DESKTOPS 4
-#define MAX_WINDOWS 16
 
-typedef struct {
-    int x, y, w, h;
-    char title[32];
-    int desktop_id; // 어떤 가상 데스크탑에 있는지
-    int is_open;
-    int is_maximized;
-    int is_dark_mode; // 다크모드 창 여부
-} compositor_window_t;
-
+// 창 배열을 외부(wm.c)에서 받아와서 해당 데스크톱만 합성하여 그림
 void compositor_init(void);
-void compositor_render(int current_desktop);
+void compositor_render_desktop(int desktop_id, wm_window_t *windows, int windows_count);
 void compositor_switch_desktop(int new_desktop);
-void compositor_snap_window(int win_id, int side); // 0: Left, 1: Right, 2: Max
+void compositor_snap_window(wm_window_t *w, int side); // 0: Left, 1: Right, 2: Max
 
 #endif
